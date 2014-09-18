a70b03e2453c2759782dc82b993c91c657ab8109
Rich Chang <rchang@signal.co>
2014-09-17 22:08:17 -0500

    replacing is done

--- replace.js
+++ replace.js
@@ -1,1 +1,19 @@
 //I think this is where all the replacing code happens
+
+//Crockford walk the dom method.
+var walk_the_DOM = function walk(node, func) {
+  func(node);
+  node = node.firstChild;
+  while (node) {
+    walk(node, func);
+    node = node.nextSibling;
+  }
+};
+
+walk_the_DOM(document.body, function(node) {
+  if(node.nodeType == 3) { //text node
+    old_text = node.nodeValue;
+    new_text = old_text.replace(/force/gi,'horse');
+    node.nodeValue = new_text;
+  }
+});
